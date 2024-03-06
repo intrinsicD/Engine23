4dead3d87f00225e55ac56f55ca94cb1a07a767d
Alexander Dieckmann <dieckman@cs.uni-bonn.de>
2024-03-06 01:02:21 +0100

    update

--- include/SafeAngle.h
+++ include/SafeAngle.h
@@ -11,17 +11,7 @@
 namespace Bcg{
     template<typename T, int N>
     double SafeAngle(const Eigen::Vector<T, N> &a, const Eigen::Vector<T, N> &b){
-        // Compute the dot product
-        double dot = a.dot(b);
-
-        // Compute the magnitudes of the vectors
-        double norm_a = a.norm();
-        double norm_b = b.norm();
-
-        // Compute the cosine of the angle
-        double cos_theta = dot / (norm_a * norm_b);
-
-        return SafeAcos(cos_theta);
+        return SafeAcos(a.dot(b) / (std::max(a.norm(), 1e-8) * std::max(b.norm(), 1e-8)));
     }
 }
 
