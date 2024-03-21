81ec22991aef62ebbe4748953b8fb74a03b0acfa
dieckman <dieckman@cs.uni-bonn.de>
2024-03-21 16:15:04 +0100

    update

---
+++ include/Rendering/ShaderProgram.h
@@ -0,0 +1,17 @@
+//
+// Created by alex on 21.03.24.
+//
+
+#ifndef ENGINE23_SHADERPROGRAM_H
+#define ENGINE23_SHADERPROGRAM_H
+
+namespace Bcg{
+    class ShaderProgram{
+    public:
+        ShaderProgram() = default;
+
+        unsigned int id;
+    };
+}
+
+#endif //ENGINE23_SHADERPROGRAM_H
--- src/Rendering/DefaultMeshRenderPass.cpp
+++ src/Rendering/DefaultMeshRenderPass.cpp
@@ -5,28 +5,24 @@
 #include "DefaultMeshRenderPass.h"
 #include "Engine.h"
 #include "Camera.h"
-#include "Transform.h"
 
 namespace Bcg {
     void DefaultMeshRenderPass::execute() const {
-        auto &camera = Engine::Context().get<Camera>();
-        auto &programs = Engine::Context().get<OpenGL::ShaderPrograms>();
-        auto program = programs["learn_opengl"];
+        auto &backend = Engine::Context().get<RenderingBackend>();
 
-        program.use();
-        program.set_mat4("view", camera.get_view().data());
-        program.set_mat4("projection", camera.get_projection().data());
+        auto &camera = Engine::Context().get<Camera>();
+        auto &program = backend.get_program("DefaultMeshRenderPass");
+        backend.use(program);
+        backend.update(camera);
 
         for (auto entity_id : m_entities) {
             auto &transform = Engine::State().get<Transform>(entity_id);
             auto &material = Engine::State().get<DefaultMeshRenderPass::Material>(entity_id);
-            auto &renderable = Engine::State().get<OpenGL::RenderableTriangles>(entity_id);
+            auto &renderable = Engine::State().get<Renderable::Mesh>(entity_id);
 
-            program.set_vec3("our_color", renderable.our_color);
-            program.set_mat4("model", transform.model.matrix().data());
-            renderable.vao.bind();
-            renderable.draw();
-            renderable.vao.release();
+            backend.update(transform);
+            backend.update(material);
+            backend.render(renderable);
         }
     }
 }
