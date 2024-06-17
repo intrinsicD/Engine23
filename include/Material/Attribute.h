c4398a8ad257deae98ed0e953f70c8706cdfe587
dieckman <dieckman@cs.uni-bonn.de>
2024-06-17 15:50:32 +0200

    Dupdate

--- include/Material/Attribute.h
+++ include/Material/Attribute.h
@@ -7,13 +7,22 @@
 
 #include <string>
 
-namespace Bcg{
-    struct Attribute{
+namespace Bcg {
+    struct Attribute {
         std::string shader_attribute_name;
         unsigned int location;
         unsigned int buffer_idx;
         unsigned int dims;
         bool normalized;
+
+        friend std::ostream &operator<<(std::ostream &stream, const Attribute &attribute) {
+            stream << "Shader Attribute Name: " << attribute.shader_attribute_name << "\n";
+            stream << "Location: " << attribute.location << "\n";
+            stream << "Buffer Idx: " << attribute.buffer_idx << "\n";
+            stream << "Dims: " << attribute.dims << "\n";
+            stream << "Normalized: " << attribute.normalized << "\n";
+            return stream;
+        }
     };
 }
 
--- src/Material/MaterialGui.cpp
+++ src/Material/MaterialGui.cpp
@@ -3,23 +3,33 @@
 //
 
 #include "MaterialGui.h"
+#include "AttributeGui.h"
+#include "Components.h"
 #include "imgui.h"
 
 namespace Bcg{
     void ComponentGui<Material>::Show(Material &material) {
         ImGui::Text("Name: %s", material.name.c_str());
         ImGui::Text("Program Index: %u", material.program_idx);
-        ImGui::Text("Attributes:");
-        for (const auto &attribute: material.attributes) {
-            ImGui::Text("%u", attribute);
+        if(ImGui::CollapsingHeader("Attributes")){
+            Components<Attribute> attributes;
+            for (auto &attribute_id: material.attributes) {
+                auto &attribute = attributes.get_instance(attribute_id);
+                if(ImGui::TreeNode(attribute.shader_attribute_name.c_str())){
+                    ComponentGui<Attribute>::Show(attribute);
+                    ImGui::TreePop();
+                }
+            }
         }
-        ImGui::Text("Uniforms:");
-        for (const auto &uniform: material.uniforms) {
-            ImGui::Text("%u", uniform);
+        if (ImGui::CollapsingHeader("Uniforms")) {
+            for (const auto &uniform: material.uniforms) {
+                ImGui::Text("%u", uniform);
+            }
         }
-        ImGui::Text("Textures:");
-        for (const auto &texture: material.textures) {
-            ImGui::Text("%u", texture);
+        if (ImGui::CollapsingHeader("Textures")) {
+            for (const auto &texture: material.textures) {
+                ImGui::Text("%u", texture);
+            }
         }
     }
 }
